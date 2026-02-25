3fa64235d2b1b60ae9f81622c0c972967e9edb6d
edumgt <kimdypm@gmail.com>
2026-02-25 10:32:41 +0900

    11111

--- server/index.js
+++ server/index.js
@@ -5,6 +5,18 @@ const { startJob, getJob, stopJob, deleteJob, listJobs }
   = require("./transcribeClient");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(express.json({ limit: "2mb" }));
 
 app.get("/health", (req, res) => res.json({ ok: true }));
